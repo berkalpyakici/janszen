f982c07564de41d88e3ab53834c6c9a432348fb2
Berk Alp Yakici <berkalp.y@gmail.com>
2018-09-15 12:37:10 -0500

    Basic Template for Index

--- web/main.py
+++ web/main.py
@@ -1,12 +1,21 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request, send_from_directory
 
 app = Flask(__name__)
 
+@app.route('/js/<path:path>')
+def send_js(path):
+    return send_from_directory('js', path)
+
+@app.route('/css/<path:path>')
+def send_css(path):
+    return send_from_directory('css', path)
+
+@app.route('/dependencies/<path:path>')
+def send_dependencies(path):
+    return send_from_directory('dependencies', path)
 
 @app.route("/")
 def index():
     return render_template("index.html")
-
 
 app.run()
