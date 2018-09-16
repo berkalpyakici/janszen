ebd5f4e8ddd12fbacf520fa7d6777ae4b3b1e690
Lucas Atayde <lsa4@rice.edu>
2018-09-15 21:10:53 -0500

    Maps!

--- web/maps.py
+++ web/maps.py
@@ -1,23 +1,63 @@
 import googlemaps
 from datetime import datetime
 
-gmaps = googlemaps.Client(key='AIzaSyAAJnnaSI6ivwFneYGiILu700jVWHDfw0c')
+#gmaps = googlemaps.Client(key='AIzaSyAAJnnaSI6ivwFneYGiILu700jVWHDfw0c')
 
 # Geocoding an address
-geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
+#geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
 
 # Look up an address with reverse geocoding
-reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
+#reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
 
 # Request directions via public transit
-now = datetime.now()
-directions_result = gmaps.directions("Sydney Town Hall",
-                                     "Parramatta, NSW",
-                                     mode="transit",
-                                     departure_time=now)
+#now = datetime.now()
+#directions_result = gmaps.directions("Sydney Town Hall",
+                                     #"Parramatta, NSW",
+                                     #mode="transit",
+                                     #departure_time=now)
+
+class maps():
+    """
+    Generates list of 
+    """
+
+    def __init__():
+        self.gmaps = googlemaps.Client(key='AIzaSyAAJnnaSI6ivwFneYGiILu700jVWHDfw0c')
+
+    def zipcode_data(zipcode):
+        """
+        Input: An integer representing a zipcode
+        Output: A list of grocery stores within the zipcode
+        """
+        return ""
+
+    def city_data():
+        """
+        Input: String representing
+        Output: A list of grocery stores within a city
+        """
+        return ""
+
+
+
+class grocery():
+
+    #lat, longitude, distance from another grocery object
+    #an initializer, a name?
+    def __init__(latitude=29.756, longitude=95.357):
+        self.lat = latitude
+
+    def within_mile(lat, long):
+        """
+        Input:
+        """
+
+#first = new grocery(some_lat, some_long)
+#first.within_mile(another_lat, another_long)
+
+#grocery(within_mile(lat))
+
 print(directions_result)
 
 
-
-def fetch_grocery():
-    
+def fetch_grocery(city):
